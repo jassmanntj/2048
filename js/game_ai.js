d5ae75c52f9fe63f586fcc2b1db7ac3f48edee05
jassmanntj <jassmanntj@email.appstate.edu>
2014-04-16 14:16:00 -0400

    Dumb AI implemented

--- js/game_ai.js
+++ js/game_ai.js
@@ -3,9 +3,21 @@ function Game_AI(manager) {
 }
 
 Game_AI.prototype.runAI = function() {
-	var i = 0;
-	while(!this.manager.isGameTerminated()) {
-		this.manager.move(i);
-		i = (i + 1)%4;
+	this.run(this, 0);
+}
+
+Game_AI.prototype.run = function(that, i) {
+	if(that.manager.isGameTerminated()) return;
+	else {
+		that.manager.move(i);
+		i = (i + 1)%4
+		setTimeout(function(){that.run(that, i)}, 100);
 	}
+}
+
+Game_AI.prototype.pausecomp = function(time) {
+  var date = new Date();
+  var curDate = null;
+  do{ curDate = new Date(); }
+  while(curDate-date < time);
 }
