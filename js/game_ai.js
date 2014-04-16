23efbc7924d5236d0224fc2eef0e270c0d49e55a
jassmanntj <jassmanntj@email.appstate.edu>
2014-04-16 15:51:16 -0400

    basic search

--- js/game_ai.js
+++ js/game_ai.js
@@ -15,9 +15,50 @@ Game_AI.prototype.dumb = function(manager, that, i) {
 	if(manager.isGameTerminated()) return;
 	else {
 		manager.move(i);
-		i = (i + 1)%4
+		i = (i + 1)%4;
 		setTimeout(function(){that.dumb(manager, that, i)}, 100);
 	}
+}
+
+Game_AI.prototype.searchAI = function() {
+	this.searchai(this.manager,this);
+}
+
+Game_AI.prototype.searchai = function(manager, that) {
+	if(manager.isGameTerminated()) return;
+	else {
+		move = that.search(1, that.manager);
+		manager.move(move[0]);
+		setTimeout(function(){that.searchai(manager, that)}, 100);
+	}
+}
+		
+		
+
+Game_AI.prototype.search = function(level, manager) {
+	var max = [0, manager.score];
+	if(level==0) return max;
+	for(var index = 0; index < 4; index++) {
+		var clone = manager.clone();
+		var moveTotal = 0;
+		clone.moveNoTile(moves[index]);
+		cells = clone.grid.availableCells();
+		for(var index2 = 0; index2 < cells.length; index2++) {
+			clone = clone.clone();
+			t2 = new Tile(cells[index2],2);
+			t4 = new Tile(cells[index2],4);
+			clone.grid.insertTile(t2);
+			moveVal = search(level-1, clone);
+			moveTotal += .9*moveVal[1];
+			clone.grid.removeTile(t2);
+			clone.grid.insertTile(t4);
+			moveVal = search(level-1, clone);
+			moveTotal += .1*moveVal[1];
+		}
+		moveTotal /= cells.length;
+		if(max[1] < moveTotal) max = [index, moveTotal];
+	}
+	return max;
 }
 
 Game_AI.prototype.pausecomp = function(time) {
--- js/game_manager.js
+++ js/game_manager.js
@@ -137,7 +137,7 @@ GameManager.prototype.move = function (direction) {
   // 0: up, 1: right, 2: down, 3: left 4: run ai
   var self = this;
   if(direction==4) {
-	this.ai.runAI();
+	this.ai.searchAI();
 	return;
   }
   if (this.isGameTerminated()) return; // Don't do anything if the game's over
@@ -198,6 +198,61 @@ GameManager.prototype.move = function (direction) {
     this.actuate();
   }
 };
+
+// Move tiles on the grid in the specified direction
+GameManager.prototype.moveNoTile = function (direction) {
+  // 0: up, 1: right, 2: down, 3: left 4: run ai
+  var self = this;
+  if (this.isGameTerminated()) return; // Don't do anything if the game's over
+
+  var cell, tile;
+
+  var vector     = this.getVector(direction);
+  var traversals = this.buildTraversals(vector);
+  var moved      = false;
+
+  // Save the current tile positions and remove merger information
+  this.prepareTiles();
+
+  // Traverse the grid in the right direction and move tiles
+  traversals.x.forEach(function (x) {
+    traversals.y.forEach(function (y) {
+      cell = { x: x, y: y };
+      tile = self.grid.cellContent(cell);
+
+      if (tile) {
+        var positions = self.findFarthestPosition(cell, vector);
+        var next      = self.grid.cellContent(positions.next);
+
+        // Only one merger per row traversal?
+        if (next && next.value === tile.value && !next.mergedFrom) {
+          var merged = new Tile(positions.next, tile.value * 2);
+          merged.mergedFrom = [tile, next];
+
+          self.grid.insertTile(merged);
+          self.grid.removeTile(tile);
+
+          // Converge the two tiles' positions
+          tile.updatePosition(positions.next);
+
+          // Update the score
+          self.score += merged.value;
+
+          // The mighty 2048 tile
+          if (merged.value === 2048) self.won = true;
+        } else {
+          self.moveTile(tile, positions.farthest);
+        }
+
+        if (!self.positionsEqual(cell, tile)) {
+          moved = true; // The tile moved from its original cell!
+        }
+      }
+    });
+  });
+  this.actuate();
+}
+
 
 // Get the vector representing the chosen direction
 GameManager.prototype.getVector = function (direction) {
